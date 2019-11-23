4119ca0dcd79cd1aaccb0f9d8b597410182636a9
Nadia TUmidajska <nadia.tumidajska@gmail.com>
2019-11-23 12:16:08 +0100

    Add Router

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import './App.css';
 import Nav from './Nav';
 import About from './About';
 import Shop from './Shop';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 
 function App() {
   return (
+    <Router>
     <div className="App">
     <Nav />
-    <About />
-    <Shop />
+    <Route path="/about" component={About}/>
+    <Route path="/shop" component={Shop}/>
     </div>
+    </Router>
   );
 
 }
