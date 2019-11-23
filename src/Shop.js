fb85666d707415c8d9a0415f9e954716899eca67
Nadia TUmidajska <nadia.tumidajska@gmail.com>
2019-11-23 16:50:48 +0100

    Add API to the project

--- src/Shop.js
+++ src/Shop.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function Shop() {
+  useEffect(() => {
+    fetchItems();
+  },[]);
+
+  const [items, setItems] = useState([]);
+
+  const fetchItems = async () => {
+    const data = await fetch('https://fortnite-api.theapinetwork.com/store/get');
+
+    const items = await data.json();
+    console.log(items.data);
+    setItems(items.data);
+  };
+
+
   return (
     <div>
-      <h1>Shop Page</h1>
+      {items.map(item => (
+        <h1 key={item.itemId}>{item.item.name}</h1>
+      ))}
     </div>
   );
 }
